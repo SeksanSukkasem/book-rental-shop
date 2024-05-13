6a8ee331c0f5b96f2a70779209df4557bb084ffb
SeksanSukkasem <143654703+SeksanSukkasem@users.noreply.github.com>
2024-05-13 19:57:02 +0700

    add book

--- src/router/productrouter.js
+++ src/router/productrouter.js
@@ -4,13 +4,13 @@ const productRouters = express.Router();
 const dataBooks = require('../data/dataBooks.json');
 
 productRouters.route("/").get((req, res) => {
-    res.render("products", {
+    res.render("booksDetail", {
         dataBooks
     });
 });
 
-productRouters.route("/:productTitle").get((req, res) => {
-    res.render("booksDetail", { dataBooks });
-});
+// productRouters.route("/:productTitle").get((req, res) => {
+//     res.render("booksDetail", { dataBooks });
+// });
 
 module.exports = productRouters;
